903933900ff47f64de47c5f2086acd2d01c78e1e
JasAlarcon <njasbleidyalarcon@ucundinamarca.edu.co>
2026-05-01 13:07:57 -0500

    primer commit proyecto MERCAM

--- database/migrations/2026_03_20_140701_create_posts_table.php
+++ database/migrations/2026_03_20_140701_create_posts_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
     public function up(): void
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+
+            $table->string('title', 150);
+            $table->string('slug', 150)->unique();
+            $table->text('content');
+            $table->enum('status', ['draft', 'published'])->default('draft');
+
             $table->timestamps();
         });
     }
 
-    /**
-     * Reverse the migrations.
-     */
     public function down(): void
     {
         Schema::dropIfExists('posts');
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,10 +2,10 @@
     <div class="container mt-5">
 
         <div class="card shadow p-4 text-center">
-            <h2 class="mb-3">Dashboard</h2>
+            <h2 class="mb-3">Bienvenido</h2>
 
             <p class="mb-3">
-                Bienvenido, <strong>{{ Auth::user()->name }}</strong>
+                , <strong>{{ Auth::user()->name }}</strong>
             </p>
 
             <div class="d-flex justify-content-center gap-3">
